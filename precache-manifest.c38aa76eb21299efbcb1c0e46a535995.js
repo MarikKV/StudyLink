173661c4905197a5f7a9cd0e573ef8cb134ad3e3
Marik1 <kosaniakm@gmail.com>
2020-02-02 01:41:23 +0200

    Updates

---
+++ precache-manifest.c38aa76eb21299efbcb1c0e46a535995.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478fe0dfd7a855616358e714b109344b",
+    "url": "/StudyLink/index.html"
+  },
+  {
+    "revision": "eb1a1862f7480a7c997b",
+    "url": "/StudyLink/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2056d5faff236f1fc2d4",
+    "url": "/StudyLink/static/css/main.ec83f9f4.chunk.css"
+  },
+  {
+    "revision": "eb1a1862f7480a7c997b",
+    "url": "/StudyLink/static/js/2.95ee33c1.chunk.js"
+  },
+  {
+    "revision": "9d0c1864f9b5cb5a4a4929eace9cee67",
+    "url": "/StudyLink/static/js/2.95ee33c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2056d5faff236f1fc2d4",
+    "url": "/StudyLink/static/js/main.436d1017.chunk.js"
+  },
+  {
+    "revision": "3d138def5e8efe2d8a3b",
+    "url": "/StudyLink/static/js/runtime-main.8668b8a7.js"
+  },
+  {
+    "revision": "cd8dbecab2b1626f1a26e1260d423bfb",
+    "url": "/StudyLink/static/media/pay.cd8dbeca.jpg"
+  },
+  {
+    "revision": "09ba2146572699509a685bade9a8a398",
+    "url": "/StudyLink/static/media/program.09ba2146.jpg"
+  }
+]);
